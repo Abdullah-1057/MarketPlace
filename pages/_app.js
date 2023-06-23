f853898036725025bb2b7271ac950e0a3bf6f031
cleverprogrammer <>
2023-06-23 14:05:20 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      desiredChainId={ChainId.Goerli}
+      chainRpc={{
+        [ChainId.Goerli]:
+          "https://goerli.infura.io/v3/9ac7f0a9e9f64b00bc5e70ff4afa404c",
+      }}
+    >
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
